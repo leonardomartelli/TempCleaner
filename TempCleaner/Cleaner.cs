c7c31c8f4d66093b9c1a2e9d83b91499307433b4
Leonardo Martelli <bwc.leonardo@gmail.com>
2020-02-08 18:28:51 -0300

    Implementing the application

---
+++ TempCleaner/Cleaner.cs
@@ -0,0 +1,66 @@
+﻿using EnumsNET;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace TempCleanner
+{
+    public class Cleaner : INotifyPropertyChanged
+    {
+
+        public Cleaner()
+        {
+            this.SetValue();
+        }
+
+        private string _pathState;
+        public string PathState
+        {
+            get => _pathState;
+
+            set
+            {
+                _pathState = value;
+                NotifyPropertyChanged(nameof(PathState));
+            }
+        }
+
+
+        [DefaultValue(eState.Dirty)]
+        private eState State { get; set; }
+
+        public void SetValue() => this.PathState = this.State.AsString(EnumFormat.Description);
+
+        public void Clean()
+        {
+            this.State = eState.Clean;
+            this.SetValue();
+            this.ClearTempFolder();
+        }
+
+        private void ClearTempFolder()
+        {
+            var tempPath = Path.GetTempPath();
+
+            foreach (var directory in Directory.GetDirectories(tempPath))
+            {
+                try { Directory.Delete(directory, true); }
+
+                catch { continue; }
+            }
+
+            foreach (var file in Directory.GetFiles(tempPath))
+            {
+                try { File.Delete(file); }
+
+                catch { continue; }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
